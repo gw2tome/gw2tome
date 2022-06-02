88a8be2b2153696469f64e2755880b2208d3dd07
jeremyknight-me <492328+jeremyknight-me@users.noreply.github.com>
2022-06-02 16:02:00 +0000

    Deploying to gh-pages from master @ aa157171570e6fbb571f6aef948287f1cb589682 🚀

---
+++ js/helpers.js
@@ -0,0 +1,10 @@
+﻿var gw2tome = {
+    localStorageGet: function (key) {
+        let value = localStorage.getItem(key);
+        return value == null ? null : JSON.parse(value);
+    },
+    localStorageSet: function (key, value) {
+        let json = JSON.stringify(value);
+        localStorage.setItem(key, json);
+    }
+}
